11021538b64aeeebeadb8f3c8e0fb1a0c24b3c7e
Terroriss <minhlong2452000@gmail.com>
2021-10-30 14:24:49 +0700

    multi lang

---
+++ js/en-pages.js
@@ -0,0 +1,1 @@
+var pagesIndex=[{index:"",title:"",tags:[]}];
---
+++ js/vi-pages.js
@@ -0,0 +1,1 @@
+var pagesIndex=[{index:"1",title:"Markdown C\u01a1 B\u1ea3n",tags:["markdown","syntax","purple-se"]},{index:"2",title:"Python C\u01a1 B\u1ea3n",tags:["python","syntax","purple-se"]},{index:"3",title:"Cheat Sheets",tags:"markdown;html;css;markup language;stylesheet;language;syntax;blue-se".split(";")}];
